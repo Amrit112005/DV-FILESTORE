a953a46d7391c28b70d8efe17e228351e214dc3d
Amrit112005 <amrit2005pay@gmail.com>
2025-01-13 01:14:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -102,7 +102,7 @@ async def start(client, message):
         else:
             return await message.reply_text(
                 text="<b>Invalid link or Expired link !</b>",
-                protect_content=True
+                protect_content=False
             )
     elif data.split("-", 1)[0] == "BATCH":
         try:
@@ -237,7 +237,7 @@ async def start(client, message):
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
                 file_id=file_id,
-                protect_content=True if pre == 'filep' else False,  
+                protect_content=False if pre == 'filep' else False,  
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
@@ -310,7 +310,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        protect_content=True if pre == 'filep' else False,
+        protect_content=False if pre == 'filep' else False,
     )
     if STREAM_MODE == True:
         g = await x.reply_text(
